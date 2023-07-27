704d980bfaa5e02e1c43d8813d6393f4d55c7c08
Ruslan Ivlev <91348490+JagerID@users.noreply.github.com>
2023-07-27 16:40:29 +0300

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,11 +4,9 @@ module.exports = {
   content: ['./src/**/*.{ts,tsx,js,jsx}', './stories/**/*.{ts,tsx,js,jsx}'],
   theme: {
     extend: {
-      width: {
         fontFamily: {
           serif: '"Inter", sans-serif',
         },
-      }
     }
   },
 }
