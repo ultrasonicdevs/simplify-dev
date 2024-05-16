437df1feb2f9a372aacb91c54952b82543c0367c
FoxyGDFD <mic.panfil0v@gmail.com>
2024-05-16 22:39:07 +0300

    refactor: rm unused packages && storybook upgrade

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,14 +3,15 @@ import './tw.css';
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/
       }
     }
-  }
+  },
+
+  tags: ['autodocs']
 };
 
 export default preview;
